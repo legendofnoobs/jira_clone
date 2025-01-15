18ff77722615b664504bf327f58d7863f21fce99
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-01-15 02:00:37 +0200

    handeled images upload

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ChangeEvent, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { createWorkspaceSchema } from "../schemas";
 import { z } from "zod";
@@ -16,6 +17,9 @@ import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useCreateWorkspace } from "../api/use-create-workspace";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Image from "next/image";
+import { ImageIcon } from "lucide-react";
 
 interface CreateWorkspaceFormProps {
     onCancel?: () => void;
@@ -24,15 +28,35 @@ interface CreateWorkspaceFormProps {
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
     const { mutate, isPending } = useCreateWorkspace();
 
+    const inputRef = useRef<HTMLInputElement>(null)
+
     const form = useForm<z.infer<typeof createWorkspaceSchema>>({
         resolver: zodResolver(createWorkspaceSchema),
         defaultValues: {
             name: "",
         }
     })
-    const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
 
-        mutate({ json: values });
+    const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
+        const finalValues = {
+            ...values,
+            image: values.image instanceof File ? values.image : "",
+        };
+
+        mutate({ form: finalValues },{
+            onSuccess: () => {
+                form.reset();
+                // todo redirect to the new workspace
+            }
+        });
+    };
+
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+
+        if (file) {
+            form.setValue("image", file)
+        }
     }
 
     return (
@@ -63,13 +87,85 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                                 )
                             }}>
                             </FormField>
+                            <FormField control={form.control} name="image" render={({ field }) => (
+                                <div className="flex flex-col gap-y-2">
+                                    <div className="flex items-center gap-x-5 bg-bl">
+                                        {field.value ? (
+                                            <div className="size-[72px] relative rounded-md overflow-hidden">
+                                                <Image alt="Logo"
+                                                    fill
+                                                    className=" object-cover"
+                                                    src={
+                                                        field.value instanceof File
+                                                            ? URL.createObjectURL(field.value)
+                                                            : field.value
+                                                    } />
+                                            </div>
+                                        ) : (
+                                            <Avatar className="size-[72px]">
+                                                <AvatarFallback>
+                                                    <ImageIcon className="size-[36px] text-neutral-400" />
+                                                </AvatarFallback>
+                                            </Avatar>
+                                        )}
+                                        <div className="flex flex-col">
+                                            <p className="text-sm">Workspace Icon</p>
+                                            <p className="text-sm text-gray-500">JPG, PNG, SVG, OR JPEG, max 1mb</p>
+                                            <input
+                                                className="hidden"
+                                                accept=".jpg, .png, .jpeg, .svg"
+                                                type="file"
+                                                ref={inputRef}
+                                                disabled={isPending}
+                                                onChange={handleImageChange}
+                                            />
+                                            {field.value ? (
+                                                <Button
+                                                    type="button"
+                                                    size="xs"
+                                                    variant="destructive"
+                                                    className="w-fit mt-2"
+                                                    disabled={isPending}
+                                                    onClick={() => {
+                                                        field.onChange(null);
+                                                        if (inputRef.current) {
+                                                            inputRef.current.value = "";
+                                                        }
+                                                    }}
+                                                >
+                                                    remove image
+                                                </Button>
+                                            ) : (
+                                                <Button
+                                                    type="button"
+                                                    size="xs"
+                                                    variant={`teritary`}
+                                                    className="w-fit mt-2"
+                                                    disabled={isPending}
+                                                    onClick={() => {
+                                                        inputRef.current?.click();
+                                                    }}
+                                                >
+                                                    upload image
+                                                </Button>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}>
+
+                            </FormField>
                         </div>
                         <div className="py-7">
                             <Separator />
                         </div>
                         <div className="flex items-center justify-between">
-                            <Button type="button" size={`lg`} variant={`secondary`} onClick={onCancel} disabled={isPending}>Cancel</Button>
-                            <Button type="submit" size={`lg`} disabled={isPending}>Create Workspace</Button>
+                            <Button type="button" size={`lg`} variant={`secondary`} onClick={onCancel} disabled={isPending}>
+                                Cancel
+                            </Button>
+                            <Button type="submit" size={`lg`} disabled={isPending}>
+                                Create Workspace
+                            </Button>
                         </div>
                     </form>
                 </Form>
--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -1,5 +1,33 @@
-import { z } from "zod"
+import { z } from 'zod';
+
+export interface ICreateWorkspace {
+    name: string;
+    image?: string;
+}
 
 export const createWorkspaceSchema = z.object({
-    name: z.string().trim().min(1, "required"),
-})
+    name: z.string().trim().min(1, "Required"),
+    image: z.union([
+        z.instanceof(File),
+        z.string().transform((value) => value === "" ? undefined : value)
+    ]).optional(),
+});
+
+export const updateWorkspaceSchema = z.object({
+    name: z.string().trim().min(1, "Must be 1 or more characters").optional(),
+    image: z
+        .union([
+            z.instanceof(File),
+            z.string().transform((value) => (value === "" ? undefined : value)),
+        ])
+        .optional(),
+});
+
+export const joinWorkspaceSchema = z.object({
+    code: z.string().trim().min(1, "Required"),
+});
+
+export const createWorkspaceFormDefaultValues: ICreateWorkspace = {
+    name: "",
+    image: "",
+};
